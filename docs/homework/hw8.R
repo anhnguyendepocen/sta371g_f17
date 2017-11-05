cc9c970698821ea5943c7f6d86b5c567f0f5be34
jaredsmurray <jmurray.1022@gmail.com>
2017-11-05 09:57:02 -0600

    add hw8

---
+++ docs/homework/hw8.R
@@ -0,0 +1,9 @@
+library(readr)
+# Load data for problem 1
+path = "https://jaredsmurray.github.io/sta371g_f17/data/"
+baseball = read_csv(paste0(path, 'baseball.csv'))
+
+# Load data for problem 2
+path = "https://jaredsmurray.github.io/sta371g_f17/data/"
+orion = read_csv(paste0(path, 'orion.csv'))
+
