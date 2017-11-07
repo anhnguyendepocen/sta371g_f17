060bb97b894afb7d305f45c477cc7379d2012bbd
jaredsmurray <jared.murray@mccombs.utexas.edu>
2017-11-07 13:32:19 -0600

    update hw8.R with relevel

--- docs/homework/hw8.R
+++ docs/homework/hw8.R
@@ -2,6 +2,12 @@ library(readr)
 # Load data for problem 1
 path = "https://jaredsmurray.github.io/sta371g_f17/data/"
 baseball = read_csv(paste0(path, 'baseball.csv'))
+# We can tell R which level should be the reference category
+# for a factor using the relevel function. The code below
+# modifis the League column so R knows we want the National
+# League to be the reference category when we fit a regression
+# model.
+baseball$League = relevel(factor(baseball$League), ref="National")
 
 # Load data for problem 2
 path = "https://jaredsmurray.github.io/sta371g_f17/data/"
